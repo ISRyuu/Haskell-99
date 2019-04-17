ac27e422bc351974abcecdc3a809fb9b71889ab3
kevin <lql0880@175game.com>
2019-04-17 16:17:45 +0800

    problem 13

--- 11-20.hs
+++ 11-20.hs
@@ -21,3 +21,17 @@ decodeModified = concat . fmap go
   where
     go (Single a) = [a]
     go (Mutiple n a) = replicate (fromInteger n) a
+
+-- problem 13
+encodeDirect :: Eq a => [a] -> [Encoding a]
+encodeDirect [] = []
+encodeDirect (x:xs) = go x 1 xs
+  where
+    go z n [] = [encoding z n]
+    go z n (x:xs)
+      | z == x = go z (n+1) xs
+      | otherwise = (encoding z n) : (go x 1 xs)
+    encoding x n
+      | n <= 0 = error "n must be greater than 0!"
+      | n == 1 = Single x
+      | otherwise = Mutiple n x
