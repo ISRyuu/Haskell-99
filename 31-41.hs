a57c7a0c15defdc269e2bf89c7b26f8423af0611
Kevin <lql9705@gmail.com>
2019-06-13 21:29:11 +0800

    problems 31-33

---
+++ 31-41.hs
@@ -0,0 +1,18 @@
+-- problem 31
+prime :: Integer -> Bool
+prime 0 = True
+prime 1 = True
+prime x = go x 2
+  where go x n
+          | n*n > x = True
+          | x `mod` n == 0 = False
+          | otherwise = True && go x (n+1)
+
+-- problem 32
+gcd' :: Integer -> Integer -> Integer
+gcd' x 0 = x
+gcd' x y = gcd y $ x `mod` y
+
+-- problem 33
+coprime :: Integer -> Integer -> Bool
+coprime = ((== 1) .) . gcd'
